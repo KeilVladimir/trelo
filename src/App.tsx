73ddbead73a4930eaa1f37a909a8384c44b3a503
KeilVladimir <Vovan32368>
2021-07-14 19:30:43 +0600

    Co

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ const ColumnsStyle = styled.div`
   justify-content: space-around;
 `;
 const App: React.FC = () => {
-  //вопрос про нейминг App на Board
   const arrayColumns = [
     {
       nameColumns: 'TODO',
