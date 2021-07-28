ef5c8f7865baa5859d4e5f1e0f466ed65a8a8e1d
KeilVladimir <Vovan32368>
2021-07-29 00:52:42 +0600

    Global fix

--- src/services/localStorage.ts
+++ src/services/localStorage.ts
@@ -1,21 +1,35 @@
-class LocalStorage {
-  private keyColumn = 'nameBoard';
-  private keyAuthor = 'AuthorName';
+import {ColumnTypes , Card , Comment , } from "../types";
 
+class LocalStorage {
+  private keyColumn = 'Board';
+  private keyAuthor = 'Author';
+  private keyCard = 'Card';
+  private keyComment = 'Comment';
   getAuthor() {
-    return localStorage.getItem(this.keyAuthor);
+    return localStorage.getItem(this.keyAuthor) || '';
   }
 
   getColumn() {
     return JSON.parse(localStorage.getItem(this.keyColumn) || '[]');
   }
-
+  getCard() {
+    return JSON.parse(localStorage.getItem(this.keyCard) || '[]');
+  }
+  getComment() {
+    return JSON.parse(localStorage.getItem(this.keyComment) || '[]');
+  }
   setAuthor(state: string) {
     localStorage.setItem(this.keyAuthor, state);
   }
 
-  setColumn(state) {
+  setColumn(state:ColumnTypes[]) {
     localStorage.setItem(this.keyColumn, JSON.stringify(state));
+  }
+  setCard(state:Card[]){
+    localStorage.setItem(this.keyCard, JSON.stringify(state));
+  }
+  setComment(state:Comment[]){
+    localStorage.setItem(this.keyComment, JSON.stringify(state));
   }
 }
 
